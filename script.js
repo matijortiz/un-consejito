5422eb0d71e1822e79c74b47fca9b64a8dc8605c
dracomagno <dracomagno@MacBook-Pro-de-dracomagno.local>
2024-08-04 22:56:53 -0300

    add Dan-image, set icons, add danjason

--- script.js
+++ script.js
@@ -54,3 +54,67 @@ document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('adviceModal');
     modal.classList.add('hidden');
 });
+
+let isDanMode = false;
+
+// MODO DAN ACTIVO
+
+function activateDanMode() {
+    isDanMode = !isDanMode;
+    const adviceImage = document.getElementById('adviceImage');
+    const keywordInput = document.getElementById('keywordInput');
+    const activateDanMode = document.getElementById('activateDanMode')
+    
+    if (isDanMode) {
+        adviceImage.src = 'img/cabeza3.png'; // Cambia la ruta a la imagen de modo Dan
+        activateDanMode.src = 'img/corazon2.png' // Cambia la ruta a la imagen de modo corazón roto
+        fetch('danconsejos.json') // Cambia la ruta al nuevo archivo JSON con consejos negativos
+            .then(response => response.json())
+            .then(data => {
+                window.danAdviceData = data; // Guarda los datos de modo Dan globalmente
+            })
+            .catch(error => {
+                console.error('Error fetching Dan advice:', error);
+            });
+    } else {
+        adviceImage.src = 'img/cabeza1.png'; // Retorna imagen original
+        activateDanMode. src ='img/corazon1.png'// R. imagen original
+        fetch('consejos.json')
+            .then(response => response.json())
+            .then(data => {
+                window.danAdviceData = null; // Elimina los datos de modo Dan
+            })
+            .catch(error => {
+                console.error('Error fetching advice:', error);
+            });
+    }
+}
+
+function getAdvice() {
+    const keywordInput = document.getElementById('keywordInput');
+    const keyword = keywordInput.value.trim().toLowerCase();
+
+    if (keyword === '') {
+        showModal('Ingresa una palabra, no seas vago.');
+        return;
+    }
+
+    const adviceData = isDanMode ? window.danAdviceData : null;
+    
+    fetch(isDanMode ? 'danconsejos.json' : 'consejos.json')
+        .then(response => response.json())
+        .then(data => {
+            const adviceEntry = (isDanMode ? adviceData : data).find(entry => entry.palabra.toLowerCase() === keyword);
+            if (adviceEntry) {
+                const randomAdvice = adviceEntry.consejos[Math.floor(Math.random() * adviceEntry.consejos.length)];
+                showModal(randomAdvice);
+            } else {
+                showModal('No se encontró ningún consejo para esa palabra.');
+            }
+        })
+        .catch(error => {
+            showModal('Error al obtener el consejo.');
+            console.error('Error fetching advice:', error);
+        });
+}
+
